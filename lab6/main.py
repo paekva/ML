b0d7e34d6ed909c934b2d6ee8b182e04754fcfac
kate <paekva@yandex.ru>
2020-03-29 17:02:18 +0300

    lab6: added IMDb dataset

---
+++ lab6/main.py
@@ -0,0 +1,26 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.utils import to_categorical
+from keras import models
+from keras import layers
+
+def vectorize(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+from keras.datasets import imdb
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
+data = np.concatenate((training_data, testing_data), axis=0)
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+data = vectorize(data)
+targets = np.array(targets).astype("float32")
+
+test_x = data[:10000]
+test_y = targets[:10000]
+train_x = data[10000:]
+train_y = targets[10000:]
+
