452ef0f6097481000b784befb39bab14f48dd24d
kate <paekva@yandex.ru>
2020-03-29 17:02:49 +0300

    lab6: added model itself

--- lab6/main.py
+++ lab6/main.py
@@ -24,3 +24,13 @@ test_y = targets[:10000]
 train_x = data[10000:]
 train_y = targets[10000:]
 
+model = models.Sequential()
+model.add(layers.Dense(50, activation = "relu", input_shape=(10000, )))
+model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation = "relu"))
+model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation = "relu"))
+model.add(layers.Dense(1, activation = "sigmoid"))
+model.compile( optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+
+history = model.fit( train_x, train_y, epochs= 2, batch_size = 500, validation_data = (test_x, test_y))
