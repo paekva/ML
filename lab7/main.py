f1a6f8b2e9bd71615a67a0f084cbf9de7f853106
kate <paekva@yandex.ru>
2020-04-02 18:50:47 +0300

    lab7: prepearing data

---
+++ lab7/main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+from keras.datasets import imdb
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
+data = np.concatenate((training_data, testing_data), axis=0)
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+X_test = data[:10000]
+y_test = targets[:10000]
+X_train = data[10000:]
+y_train = targets[10000:]
+
+max_review_length = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
