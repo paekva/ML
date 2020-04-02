9a9d72431bfad72126a028175630503124909d9b
kate <paekva@yandex.ru>
2020-04-02 19:00:35 +0300

    added model architecture

--- lab7/main.py
+++ lab7/main.py
@@ -6,6 +6,8 @@ from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
+top_words = 5000
+
 from keras.datasets import imdb
 (training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
 data = np.concatenate((training_data, testing_data), axis=0)
@@ -20,3 +22,13 @@ max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 
+embedding_vecor_length = 32
+model = Sequential()
+model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
