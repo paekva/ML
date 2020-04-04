34c0d96d5513b7e8e1613e3663933fa9c5ae7dc5
kate <paekva@yandex.ru>
2020-04-04 15:49:41 +0300

    lab8: more data preparation: making input data vectors for model

--- lab8/main.py
+++ lab8/main.py
@@ -15,3 +15,17 @@ char_to_int = dict((c, i) for i, c in enumerate(chars))
 
 n_chars = len(raw_text)
 n_vocab = len(chars)
+
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+X = X / float(n_vocab)
+y = np_utils.to_categorical(dataY)
