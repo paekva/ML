fea1bc58e0d088d556b6f60198143d5b3492b8a0
kate <paekva@yandex.ru>
2020-04-04 19:11:45 +0300

    lab7: added drawing method

--- lab7/main.py
+++ lab7/main.py
@@ -5,7 +5,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
-
+import draw
 from keras.datasets import imdb
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
@@ -21,7 +21,8 @@ model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+H = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+draw.draw_graphics(H)
