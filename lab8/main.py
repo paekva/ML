138e23f1c9f7f28776e468594959605f176b90e6
kate <paekva@yandex.ru>
2020-04-04 15:48:47 +0300

    lab8: added file with data, started its preparing process

---
+++ lab8/main.py
@@ -0,0 +1,17 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+filename = "wonderland.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+
+n_chars = len(raw_text)
+n_vocab = len(chars)
